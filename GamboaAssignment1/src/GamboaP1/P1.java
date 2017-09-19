179ce797bf4cb5e3ebeca65774538e672517fb34
anagam5 <ggambo1@students.towson.edu>
2017-09-19 13:15:27 -0400

    Added P1 and P2 descriptions to their respective java files

--- GamboaAssignment1/src/GamboaP1/P1.java
+++ GamboaAssignment1/src/GamboaP1/P1.java
@@ -3,6 +3,17 @@ package GamboaP1;
  *
  * @author anagam5
  */
+
+/*
+1. Write a Java program that asks the user to enter a single hex value H and a small text string T.
+1.1 The program first uses AES in counter mode as follows:
+Encrypts the 16-byte value S consisting of T padded on the right with zeros (if necessary) with the key K, 
+where K is the 16-byte value consisting of H repeated 32 times, and the counter is initialized to the 16-byte 
+value consisting of all zeros. It then prints the encrypted result in hex.
+1.2 Next, it authenticates the original 16-byte value S above using HMAC SHA256 with the same key K above 
+and with the IV for HMAC SHA 256 taken to be the 32-byte value S concatenated with itself. It then 
+prints the final computed HMAC result in hex.
+*/
 public class P1 {
     
 }
--- GamboaAssignment1/src/GamboaP2/P2.java
+++ GamboaAssignment1/src/GamboaP2/P2.java
@@ -3,6 +3,27 @@ package GamboaP2;
  *
  * @author anagam5
  */
+
+/*
+2. Write a Java socket program to do a Diffie-Hellman (DH) key exchange as follows. 
+The client A and server B must perform the given actions in order as specified below:
+Client A actions
+Asks user at A to enter prime q as a string
+Asks user at A to enter a possible primitive root of q (alpha) as a string
+Test if alpha is a primitive root; if not, reenter alpha and test
+Asks user at A to enter A’s private key X(A) as an integer
+Uses Diffie-Hellman to generate A’s public key Y(A) and prints Y(A)
+Uses the socket to send q, alpha, A’s public key Y(A), and A’s email address to the server B
+Uses the socket to receive the information sent by the server B 
+Uses Diffie-Hellman to generate the shared key K and prints K.
+Server B actions
+Uses the socket to receive q, alpha, A’s public key Y(A), and A’s email address from A
+Ask user at B to enter B’s private key X(B) as a string
+Uses Diffie-Hellman to generate B’s public key Y(B) and print Y(B)
+Uses the socket to send server B’s public key Y(B) and email address to the client A 
+Uses Diffie-Hellman to generate the shared key K and prints K.
+*/
+
 public class P2 {
     
 }
